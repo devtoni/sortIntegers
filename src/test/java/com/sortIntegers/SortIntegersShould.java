87e807755754e6085a5ae69717069b05ac1e0bd1
devtoni <devtonirm@gmail.com>
2018-10-27 23:21:46 +0200

    test(sortIntegers): first red

---
+++ src/test/java/com/sortIntegers/SortIntegersShould.java
@@ -0,0 +1,26 @@
+package com.sortIntegers;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+
+
+public class SortIntegersShould {
+    @Test
+    public void sort_an_integers_list(){
+        assertThat(sortIntegers(intList()), is(intList()));
+    }
+
+    private List<Object> intList() {
+        return Arrays.asList();
+    }
+
+    private List<Integer> sortIntegers(List<Object> asList) {
+        return null;
+    }
+}
