ab7e0e9eea6667b3503463e86255a3934ff01e10
devtoni <devtonirm@gmail.com>
2018-10-27 23:42:29 +0200

    test(sortIntegers): temp variable

--- src/test/java/com/sortIntegers/SortIntegersShould.java
+++ src/test/java/com/sortIntegers/SortIntegersShould.java
@@ -24,11 +24,19 @@ public class SortIntegersShould {
     private Object[] addParameters() {
         return new Object[]{
                 new Object[]{Arrays.asList(), Arrays.asList()},
-                new Object[]{Arrays.asList(1), Arrays.asList(1)}
+                new Object[]{Arrays.asList(1), Arrays.asList(1)},
+                new Object[]{Arrays.asList(2,1), Arrays.asList(1, 2)}
         };
     }
 
     private List<Integer> sortIntegers(List<Integer> asList) {
+        if (asList.size() > 1) {
+            Integer temp = asList.get(0);
+            if (temp > asList.get(1)) {
+                asList.set(0, asList.get(1));
+                asList.set(1, temp);
+            }
+        }
         return asList;
     }
 }
