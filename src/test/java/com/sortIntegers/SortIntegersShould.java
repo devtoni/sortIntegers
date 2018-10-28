e9530c82502780ac808b256be594ef15bd5a644b
devtoni <devtonirm@gmail.com>
2018-10-28 10:34:36 +0100

    test(refactor): extracting methods and use for loop

--- src/test/java/com/sortIntegers/SortIntegersShould.java
+++ src/test/java/com/sortIntegers/SortIntegersShould.java
@@ -17,7 +17,7 @@ public class SortIntegersShould {
 
     @Test
     @Parameters(method = "addParameters")
-    public void sort_an_integers_list(List<Integer> unsorted,List<Integer> sorted){
+    public void sort_an_integers_list(List<Integer> unsorted, List<Integer> sorted) {
         assertThat(sortIntegers(unsorted), is(sorted));
     }
 
@@ -25,26 +25,26 @@ public class SortIntegersShould {
         return new Object[]{
                 new Object[]{Arrays.asList(), Arrays.asList()},
                 new Object[]{Arrays.asList(1), Arrays.asList(1)},
-                new Object[]{Arrays.asList(2,1), Arrays.asList(1, 2)},
-                new Object[]{Arrays.asList(3,1,2), Arrays.asList(1, 2, 3)}
+                new Object[]{Arrays.asList(2, 1), Arrays.asList(1, 2)},
+                new Object[]{Arrays.asList(3, 1, 2), Arrays.asList(1, 2, 3)}
         };
     }
 
     private List<Integer> sortIntegers(List<Integer> asList) {
-        if (asList.size() > 1) {
-            Integer temp = asList.get(0);
-            if (temp > asList.get(1)) {
-                asList.set(0, asList.get(1));
-                asList.set(1, temp);
-            }
-        }
-        if (asList.size() > 2) {
-            Integer temp = asList.get(1);
-            if (temp > asList.get(2)) {
-                asList.set(1, asList.get(2));
-                asList.set(2, temp);
-            }
+        for (int index = 0; asList.size() > index + 1; index++) {
+            if (outOfOrder(asList, index))
+                swap(asList, index);
         }
         return asList;
+    }
+
+    private boolean outOfOrder(List<Integer> asList, int index) {
+        return asList.get(index) > asList.get(index + 1);
+    }
+
+    private void swap(List<Integer> asList, int index) {
+        Integer temp = asList.get(index);
+        asList.set(index, asList.get(index + 1));
+        asList.set(index + 1, temp);
     }
 }
